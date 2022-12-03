73cbbc508532efa23fe57df2f7d0945cdf67c1f5
arat <aratislamsafol@gmail.com>
2022-12-03 20:25:39 +0600

    Private Route Problem

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ import Login from './components/pages/Login';
 import Quiz from './components/pages/Quiz';
 import Signup from './components/pages/Signup';
 import Success from './components/pages/Success';
+import PrivateRoute from './components/PrivateRoute';
+// import PublicRoute from './components/PublicRoute';
+// import React, { Fragment } from 'react';
 import { AuthProvider } from './contexts/AuthContext';
+// import React, {Fragment} from 'react';
 function App() {
     return (
         <div className="App">
             <Router>
                 <AuthProvider>
-                    <Layout>
-                        <Routes>
-                            <Route exact path='/' element={<Home/>}></Route>
-                            <Route exact path='/signup' element={<Signup/>}></Route>
-                            <Route exact path='/login' element={<Login/>}></Route>
-                            <Route exact path='/quiz' element={<Quiz/>}></Route>
-                            <Route exact path='/success' element={<Success />}></Route>
-                        </Routes>
-                    </Layout>
+                    {/* <Fragment> */}
+                        <Layout>
+                            <Routes>
+                                <Route exact path='/' element={<Home/>}></Route>
+                                <Route exact path='/signup' element={<Signup/>}></Route>
+                                <Route exact path='/login' element={<Login/>}></Route>
+                               
+                                <Route exact path="/quiz" element={<PrivateRoute>
+                                    <Route  element={<Quiz/>} />
+                                    </PrivateRoute>}></Route>
+                                
+                                
+                                {/* <Route exact path="/quiz" element={<Quiz/>} /> */}
+                                <Route exact path='/success' element={<Success />}></Route>
+                            </Routes>
+                        </Layout>
+                    {/* </Fragment> */}
                 </AuthProvider>
             </Router>
         </div>
